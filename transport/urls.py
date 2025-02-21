c1d91ae605c8132f1e7bf1c8d5d31f7c8d3fb6c7
ynikolaenko <y.nikolaenko@gmail.com>
2025-02-21 11:46:14 +0200

    t2

--- transport/urls.py
+++ transport/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import cars_list
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('transport.urls')),  # Должно быть 'transport.urls', а не 'transport_accounting.urls'
+    path('', cars_list, name='cars_list'),
 ]
-
